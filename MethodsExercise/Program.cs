6e8fabfe8c525117cb1b8b4402cf639461cabbcd
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-19 11:20:38 -0500

    excercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace MethodsExercise
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("what is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("what is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("what is your favorite band?");
+            var band = Console.ReadLine();
+            Console.WriteLine($"Hello my name is {name}, when I went to africa there was a big {animal} on the road, the weird thing is that it was {color}, then it run to eat me, but I was saved by a helicopter driven by {band} singer");
+
+            
         }
     }
 }

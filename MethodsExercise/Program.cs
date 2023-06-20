517ffc0003c3645bb293e882177aebb136c4145c
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-20 00:01:09 -0500

    completed excercises with bonus

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -6,8 +6,19 @@ namespace MethodsExercise
     public class Program
     {
         static void Main(string[] args)
+
+
         {
-            int answer1 = Add(6, 7);  //excercise 1
+            int sum1 = Sums(2, 4); //extra excercise
+            Console.WriteLine("Sum of 2 and 4: " + sum1);
+            int sum2 = Sums(2, 4, 6);
+            Console.WriteLine("Sum of 2,4 and 6: "+ sum2);
+            int sum3 = Sums(1, 1, 1, 1, 1);
+            Console.WriteLine("Sum of 1,1,1,1 and 1: "+  sum3);
+
+
+
+            int answer1 = Add(6, 7);  //excercise 2
             Console.WriteLine(answer1);
             int answer2 = Subtract(9, 7);
             Console.WriteLine(answer2);
@@ -50,7 +61,17 @@ namespace MethodsExercise
             return num1 / num2;
 
         }
+        public static int Sums(params int[] nums) // extra excercise
+        {
+            int sum = 0;
+            foreach (int num in nums)
+            {
+                sum += num;
+            }
+            return sum;
+        }
         
+
     }
 }
 
